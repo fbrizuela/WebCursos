03a1a2eac2205681a61e57e41986b48fde51a15b
Facundo Brizuela <brizuelafacundoingacio@gmail.com>
2020-11-23 19:15:54 -0300

    Auditoria, SoftDeleted

---
+++ AnexUniv/04-Common/Model/Domain/CourseLessonLearnedsPerStudent.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Common.CustomFilters;
+using Model.Auth;
+using Model.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain
+{
+    public class CourseLessonLearnedsPerStudent : AuditEntity, ISoftDeleted
+    {
+        public int Id { get; set; }
+        public LessonsPerCourse Lesson { get; set; }
+        public int LessonId { get; set; }
+        
+      
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
